18175fe6b1c744c66b3732f0a86587dc54532df0
lianghp <lianghp2018@gmail.com>
2019-07-10 12:24:11 +0800

    函数基础练习

---
+++ 函数/01_体验函数.py
@@ -0,0 +1,26 @@
+"""
+函数先定义后使用,先调用会报错:
+    NameError: name 'sel_func' is not defined
+
+定义函数的语法:
+函数前后都需要空两行
+
+
+def 函数名(参数):
+    代码1
+    代码2
+    .....
+
+
+函数的调用:
+函数名(参数)
+"""
+
+
+def sel_func():
+    print('qewrqwer')
+    print('you')
+
+
+# 调用函数
+sel_func()
