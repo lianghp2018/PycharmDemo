3c098cafccdea59d0738c6fc077329832f8b0319
lianghp <lianghp2018@gmail.com>
2019-07-12 10:50:56 +0800

    函数基础练习

--- 函数/05_函数返回值作为参数传递.py
+++ 函数/05_函数返回值作为参数传递.py
@@ -2,9 +2,21 @@ def func_result():
     return 10
 
 
+# 多返回值默认返回的是元组
+# 也可以直接返回列表,字典,集合
+def func_results():
+    # return [1, 2]
+    # return {'tom', 20}
+    # return {'name': 'Lily', 'age': 20}
+    return 10, 20
+
+
 def func_print(result):
     print(result)
 
 
 result_of_func = func_result()
+results_of_func = func_results()
+print(type(results_of_func))
 func_print(result_of_func)
+func_print(results_of_func)
