672a508230d0c9f798153e5a2b156fe32aeaaa73
lianghp <lianghp2018@gmail.com>
2019-07-16 21:25:56 +0800

    递归练习

---
+++ 递归/04_lambda表达式的参数形式.py
@@ -0,0 +1,22 @@
+# 无参数形式
+fn1 = lambda: 100
+print(fn1())
+
+# 一个参数
+fn2 = lambda a: a
+print(fn2('Hello World'))
+
+# 默认参数
+fn3 = lambda a, b, c=100: a + b + c
+print(fn3(10, 20))
+print(fn3(100, 200, 300))
+
+# 可变参数: *args 返回值为元组
+fn4 = lambda *args: args
+print(fn4(100, 200, 'Hello'))
+
+# 可变参数: **kwargs  返回值为字典
+fn5 = lambda **kwargs: kwargs
+print(type(fn5(time='100', name=200, age=300)))
+
+
