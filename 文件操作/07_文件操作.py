a7bcff3304f0f259b11731df24430164b6655dc1
lianghp <lianghp2018@gmail.com>
2019-07-27 12:49:00 +0800

    文件操作练习

--- 文件操作/07_文件操作.py
+++ 文件操作/07_文件操作.py
@@ -5,7 +5,30 @@
 import os
 
 # rename(): 重命名
-os.rename('2.txt', '1.txt')
+# os.rename('2.txt', '1.txt')
 
 # remove():删除文件
-os.remove('1_copy.txt')
+# os.remove('1_copy.txt')
+
+# mkdir():创建文件夹
+# os.mkdir('aa')
+
+# rmdir():删除文件夹
+# os.rmdir('aa')
+
+# getcwd()获取当前文件夹
+# print(os.getcwd())
+
+# chdir():改变目录路径
+# 在当前目录下创建文件夹
+# os.mkdir('aa')
+# os.mkdir('bb')
+
+# 在指定目录下创建文件夹
+# 1.切换到指定目录
+os.chdir('aa')
+# 2.创建文件夹
+os.mkdir('cc')
+
+# listdir(): 获取目录列表
+# print(os.listdir())
