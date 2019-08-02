f6b314f931f5610130d644cec2e6a3b3e6c4987d
lianghp <lianghp2018@gmail.com>
2019-08-02 15:43:45 +0800

    多态练习

---
+++ 多态/02_体验多态.py
@@ -0,0 +1,32 @@
+class Dog(object):
+    def work(self):
+        print('指哪打哪')
+
+
+class ArmyDog(Dog):
+    def work(self):
+        print('追击敌人.....')
+
+
+class DrugDog(Dog):
+    def work(self):
+        print('追查毒品....')
+
+# 传入不同的对象,执行不同的代码,即不同的work函数
+class Person(object):
+    def work_with_dog(self, dog):
+        dog.work()
+
+
+ad = ArmyDog()
+dd = DrugDog()
+
+daqiu = Person()
+daqiu.work_with_dog(ad)
+daqiu.work_with_dog(dd)
+
+
+
+
+
+
